d870408fc2edbbdec8ed77ef02eeda3bf1561559
Alkiro-RA <alkiewiczroger@gmail.com>
2023-03-09 14:59:49 +0100

    First Commit: - Done:     - Repo to store applications that will cover PHP essential concepts     - Docker container php-fundamentals, with a PHP web app configured in it     - Template on index.php, that explains how "things work": Each page is a mini-app itself - Todo:     - Fundamental lessons

---
+++ php-fundamentals/app/public/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta  charset="UTF-8">
+        <title>PHP Fundamentals</title>
+    </head>
+    <body>
+        <h1>PHP Fundamentals</h1>
+        <p><b>Concept paths (copy & paste):</b></p>
+        <ul>
+            <li>Hello World</li>
+            <label><i>localhost/lesson/hello-world.php</i></label>
+
+            <li>Introduce yourself</li>
+            <label><i>localhost/lesson/user-input.php</i></label>
+
+            <li>Calculate circle's area</li>
+            <label><i>localhost/lesson/variables-and-calculations.php</i></label>
+            
+            <li>Calculate factorial</li>
+            <label><i>localhost/lesson/control-structures.php</i></label>
+            
+            <li>Find min-max</li>
+            <label><i>localhost/lesson/arrays.php</i></label>
+            
+            <li>Detect palindrome</li>
+            <label><i>localhost/lesson/functions.php</i></label>
+        </ul>
+    </body>
+</html>
+
+<!-- 
+Operators:
+    Create a program that calculates the area of a circle using the radius provided by the user.
+    Create a program that determines whether a given year is a leap year or not.
+Control structures:
+    Create a program that generates a random number between 1 and 100 and asks the user to guess the number. Use a while loop to keep asking the user for guesses until they guess the correct number.
+    Create a program that calculates the factorial of a number provided by the user using a for loop.
+Functions:
+    Create a program that calculates the sum of two numbers provided by the user using a function.
+    Create a program that checks whether a given string is a palindrome or not using a function.
+Arrays:
+    Create a program that stores a list of your favorite foods in an array and displays them on the screen.
+    Create a program that finds the largest and smallest numbers in an array of numbers provided by the user.
+-->
